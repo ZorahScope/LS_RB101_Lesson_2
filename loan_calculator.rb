b6bc44093c75cea9dfa0360e34d141edc24766fd
Eli <mojojoflow@pm.me>
2023-01-30 20:23:37 -0600

    Finish MVP of loan calculator

--- loan_calculator.rb
+++ loan_calculator.rb
@@ -3,7 +3,7 @@ def prompt(message)
 end
 
 def calculate_monthly_interest(apr)
-  apr / 12
+  (apr/100) / 12.0
 end
 
 def calculate_loan_duration_months(years, months = 0)
@@ -15,27 +15,37 @@ def calculate_monthly_payment(loan, monthly_interest, loan_duration_months)
     (monthly_interest / (1 - (1 + monthly_interest)**(-loan_duration_months)))).round(2)
 end
 
+# Receive Inputs
 prompt("Welcome to the loan calculator")
 puts("-" * 40)
 
 prompt("Please enter your loan ammount: ")
 loan_amount = gets.chomp.to_i
 
-prompt("Please enter your APR in decimal form: ")
+prompt("Please enter your APR: ")
+prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
 apr = gets.chomp.to_f
 
 prompt("Please enter your loan duration in years: ")
 loan_duration_years = gets.chomp.to_i
 
-prompt("Calculating...")
-sleep 2
+prompt("Calculating...\n")
+sleep 1
 
 puts(("-" * 20) + " Results " + ("-" * 20))
 
-p monthly_interest = calculate_monthly_interest(apr)
+monthly_interest = calculate_monthly_interest(apr)
 
-p loan_duration_months = calculate_loan_duration_months(loan_duration_years)
+loan_duration_months = calculate_loan_duration_months(loan_duration_years)
 
-p monthly_payment = calculate_monthly_payment(loan_amount, monthly_interest, loan_duration_months)
+monthly_payment = calculate_monthly_payment(loan_amount, monthly_interest, loan_duration_months)
 
-p total_of_monthly_payments = monthly_payment * loan_duration_months
+total_of_monthly_payments = monthly_payment * loan_duration_months
+
+results = <<-MSG
+  Payment Every Month: #{monthly_payment}
+  Total of #{loan_duration_months} Payments: #{total_of_monthly_payments}
+  Monthly Interest Rate: #{(monthly_interest * 100.to_f)}%
+MSG
+
+puts(results)
